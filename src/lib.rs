ae058f95c426d5227def719030e4379e4b6cc0ee
Samuel Maier <samuel.maier2@hotmail.de>
2024-09-06 18:04:22 +0200

    fix a few issues

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use embassy_time::Timer;
 use wasm_bindgen::prelude::*;
 
+const SEC_WAIT: u64 = 1;
+
 #[embassy_executor::task]
 async fn ticker_main() {
     loop {
         log::info!("tick from main thread");
 
-        Timer::after_secs(1).await;
+        Timer::after_secs(SEC_WAIT).await;
     }
 }
 
@@ -14,8 +16,8 @@ async fn ticker_main() {
 async fn ticker_webworker() {
     loop {
         log::info!("tick from webworker");
-
-        Timer::after_secs(1).await;
+        Timer::after_secs(SEC_WAIT).await;
+        // wasm_thread::sleep(std::time::Duration::from_secs(1));
     }
 }
 
@@ -24,14 +26,12 @@ fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     wasm_logger::init(wasm_logger::Config::default());
     let executor = Box::leak(Box::new(embassy_executor::Executor::new()));
-    // Don't ask me why they're are named differently. They do the same AFAICT.
     executor.start(|spawner| {
         spawner.spawn(ticker_main()).unwrap();
-    });    
+    });
     wasm_thread::spawn(|| {
-        log::info!("Hello from thread");
+        log::info!("Hello from webworker");
         let executor = Box::leak(Box::new(embassy_executor::Executor::new()));
-        // Don't ask me why they're are named differently. They do the same AFAICT.
         executor.start(|spawner| {
             spawner.spawn(ticker_webworker()).unwrap();
         });
